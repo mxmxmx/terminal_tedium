415d83e4b96e32c2654a980e822aa43baa7c8499
mxmxmx <mxxstadler@gmail.com>
2014-12-02 23:30:58 +0100

    xxx

---
+++ software/pullup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# pull up GPIO23-25 (tact switches)
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN, pull_up_down = GPIO.PUD_UP)

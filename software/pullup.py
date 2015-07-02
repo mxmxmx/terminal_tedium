6fe07be711e522a0035afaf9773e14267257e8de
mxmxmx <mxxstadler@gmail.com>
2015-07-02 15:07:49 +0200

    xxx

--- software/pullup.py
+++ software/pullup.py
@@ -9,3 +9,5 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 GPIO.setup(25, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
